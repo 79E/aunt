ed859916d0daf676d61333c027796a414e184bf0
79E <5980844@qq.com>
2022-08-02 12:52:44 +0800

    update: 更新Avatar Demo演示图片地址

--- src/components/avatar/demos/demo.tsx
+++ src/components/avatar/demos/demo.tsx
@@ -7,15 +7,15 @@ function Demo (){
     return <div className="demo-avatar">
         <DemoBlock title="头像尺寸">
             <Space wrap>
-                <Avatar size="small" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/099247.jpg"></Avatar>
-                <Avatar size="normal" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/099247.jpg"></Avatar>
-                <Avatar size="large" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/099247.jpg"></Avatar>
+                <Avatar size="small" url="https://img.iwave.net.cn/qm/a2e0b87a04da3c7818a0fe3fa1700991.png"></Avatar>
+                <Avatar size="normal" url="https://img.iwave.net.cn/qm/a2e0b87a04da3c7818a0fe3fa1700991.png"></Avatar>
+                <Avatar size="large" url="https://img.iwave.net.cn/qm/a2e0b87a04da3c7818a0fe3fa1700991.png"></Avatar>
             </Space>
         </DemoBlock>
         <DemoBlock title="头像形状" >
             <Space wrap>
-                <Avatar shape="square" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/099247.jpg"></Avatar>
-                <Avatar shape="round" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/099247.jpg"></Avatar>
+                <Avatar shape="square" url="https://img.iwave.net.cn/qm/a2e0b87a04da3c7818a0fe3fa1700991.png"></Avatar>
+                <Avatar shape="round" url="https://img.iwave.net.cn/qm/a2e0b87a04da3c7818a0fe3fa1700991.png"></Avatar>
             </Space>
         </DemoBlock>
         <DemoBlock title="头像类型" >
@@ -23,16 +23,16 @@ function Demo (){
                 <Avatar></Avatar>
                 <Avatar icon={<AuntIconMeh />}></Avatar>
                 <Avatar>头</Avatar>
-                <Avatar url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/099247.jpg"></Avatar>
+                <Avatar url="https://img.iwave.net.cn/qm/a2e0b87a04da3c7818a0fe3fa1700991.png"></Avatar>
             </Space>
         </DemoBlock>
         <DemoBlock title="图片填充模式" >
             <Space wrap>
-            <Avatar fit="contain" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/WechatIMG675.jpeg"></Avatar>
-            <Avatar fit="cover" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/WechatIMG675.jpeg"></Avatar>
-            <Avatar fit="fill" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/WechatIMG675.jpeg"></Avatar>
-            <Avatar fit="none" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/WechatIMG675.jpeg"></Avatar>
-            <Avatar fit="scale-down" url="https://cdn.jsdelivr.net/gh/duogongneng/OneMyBlogImg@master/WechatIMG675.jpeg"></Avatar>
+            <Avatar fit="contain" url="https://img.iwave.net.cn/qm/1635a93f517fba721a8c460bdab64bc3.jpeg"></Avatar>
+            <Avatar fit="cover" url="https://img.iwave.net.cn/qm/1635a93f517fba721a8c460bdab64bc3.jpeg"></Avatar>
+            <Avatar fit="fill" url="https://img.iwave.net.cn/qm/1635a93f517fba721a8c460bdab64bc3.jpeg"></Avatar>
+            <Avatar fit="none" url="https://img.iwave.net.cn/qm/1635a93f517fba721a8c460bdab64bc3.jpeg"></Avatar>
+            <Avatar fit="scale-down" url="https://img.iwave.net.cn/qm/1635a93f517fba721a8c460bdab64bc3.jpeg"></Avatar>
             </Space>
         </DemoBlock>
         <DemoBlock title="颜色" >
