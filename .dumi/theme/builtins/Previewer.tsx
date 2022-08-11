19ecb130e18adeb173408d8573e86c21f53931fa
79E <5980844@qq.com>
2022-08-11 16:26:13 +0800

    docs: 修改文档内容代码展示模式

--- .dumi/theme/builtins/Previewer.tsx
+++ .dumi/theme/builtins/Previewer.tsx
@@ -8,7 +8,6 @@ import React, {
 import { context } from 'dumi/theme'
 import type { IPreviewerProps } from 'dumi-theme-default/es/builtins/Previewer'
 import Previewer from 'dumi-theme-default/es/builtins/Previewer'
-import { debounce } from 'lodash'
 import './Previewer.less'
 
 export const ACTIVE_MSG_TYPE = 'dumi:scroll-into-demo'
@@ -21,7 +20,10 @@ export default (props: IPreviewerProps) => {
   )
   const [isActive, setIsActive] = useState(false)
   const isInactive = meta.mobile !== false && !isActive
+  const isHidden = props.hidden == true ? true : false
+  
   const activeSelf = useCallback(() => {
+    if(!isHidden) return
     window.postMessage(
       {
         type: ACTIVE_MSG_TYPE,
@@ -29,6 +31,7 @@ export default (props: IPreviewerProps) => {
           identifier: props.identifier,
           demoUrl: props.demoUrl,
           simulator: props.simulator,
+          hidden: isHidden
         }),
       },
       '*'
@@ -41,45 +44,17 @@ export default (props: IPreviewerProps) => {
     /* istanbul ignore next */
     if (!meta.title) return
 
-    const isFirstDemo =
-      document.querySelector('.__dumi-default-mobile-previewer') === ref.current
-    const handler = debounce(() => {
-      const navHeight = 64
-      // for active by previous title anchor
-      const scrollTop = document.documentElement.scrollTop
-      const refElmTop = ref.current?.getBoundingClientRect().top + scrollTop
-      // do not offset front if the previous element is same the previewer
-      const edgeOffset =
-        ref.current?.previousElementSibling?.className ===
-        ref.current?.className
-          ? navHeight
-          : navHeight + 128
-
-      // post message if scroll into current demo
-      if (
-        // fallback to first demo
-        (isFirstDemo && scrollTop < refElmTop) ||
-        // detect scroll position
-        (scrollTop + edgeOffset > refElmTop &&
-          scrollTop + navHeight < refElmTop + ref?.current?.offsetHeight)
-      ) {
-        activeSelf()
-      } else {
-        setIsActive(false)
-      }
-    }, 50)
-
+    if(isHidden){
+      activeSelf()
+    }
+    
     if (
       // only render mobile phone when screen max than 960px
       window?.outerWidth > 960 &&
       // do not disable mobile simulator
       meta.mobile !== false
     ) {
-      // active source code wrapper if scroll into demo
-      handler()
-      window.addEventListener('scroll', handler)
 
-      // rewrite props for device mode
       setPreviewerProps(
         Object.assign({}, props, {
           // omit iframe
@@ -98,8 +73,7 @@ export default (props: IPreviewerProps) => {
       // use standard mode if screen min than 960px
       setPreviewerProps(props)
     }
-
-    return () => window.removeEventListener('scroll', handler)
+    return () => {}
   }, [props, meta])
 
   useEffect(() => {
@@ -114,15 +88,15 @@ export default (props: IPreviewerProps) => {
     }
 
     window.addEventListener('message', handler)
-
+  
     return () => window.removeEventListener('message', handler)
   })
 
   return (
     <div
-      className={
-        meta.mobile !== false ? '__dumi-default-mobile-previewer' : null
-      }
+      className={`
+      ${meta.mobile !== false ? '__dumi-default-mobile-previewer' : null} ${isHidden ? '__dumi-default-previewer--hidden' : ''}
+      `}
       onClick={() => {
         if (isInactive) {
           activeSelf()
