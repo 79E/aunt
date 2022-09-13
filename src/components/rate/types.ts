a1be688cbf5374675feb3a029b483902454c81ed
79E <5980844@qq.com>
2022-09-13 10:25:46 +0800

    docs(Rate): 对评分类型的定义进行调整和删除

--- src/components/rate/types.ts
+++ src/components/rate/types.ts
@@ -11,21 +11,37 @@ export interface RateProps extends BaseTypeProps {
    * @default 4
    */
   gutter?: number | string;
-  /** 是否为只读状态，只读状态下无法修改评分	 */
+  /** 
+   * 是否为只读状态，只读状态下无法修改评分	 
+   */
   readonly?: boolean;
-  /** 是否禁用评分	 */
+  /** 
+   * 是否禁用评分	 
+   */
   disabled?: boolean;
-  /** 禁用时的颜色	 */
-  disabledColor?: string;
+  /** 
+   * 是否可以清除所选择分数 
+   */
   allowClear?: boolean;
-  /** 是否允许半选	 */
+  /** 
+   * 是否允许半选	 
+   */
   allowHalf?: boolean;
-  /** 未选中时的颜色	 */
+  /** 
+   * 未选中时的颜色	 
+   */
   voidColor?: string;
-  /** 当前分值 */
+  /** 
+   * 当前分值 
+   */
   value?: number;
+  /** 
+   * 默认分值 
+   */
   defaultValue?: number;
-  /** 当前分值变化时触发的事件	 */
+  /** 
+   * 当前分值变化时触发的事件	 
+   */
   onChange?: (value: number) => void;
   /**
    * 选中时的图标名称或图片链接
