c9e19b654f3962b317c3e13cf387354b90fa02a8
79E <5980844@qq.com>
2022-10-18 16:26:58 +0800

    chore: 优化构建打包发布相关规范和流程

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,23 @@
 module.exports = {
-  semi: false,
+  // 一行代码的最大字符数，默认是80
+  printWidth: 100,
+  // 每行末尾自动添加分号
+  semi: true,
   trailingComma: 'es5',
+  // tab缩进大小,默认为2
   tabWidth: 2,
+  // 字符串使用单引号
   singleQuote: true,
-  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  // 在jsx文件中的引号需要单独设置
   jsxSingleQuote: true,
+  // object对象中key值是否加引号 （quoteProps: "<as-needed|consistent|preserve>"）as-needed只有在需求要的情况下加引号，consistent是有一个需要引号就统一加，preserve是保留用户输入的引号
   quoteProps: 'preserve',
+  // 箭头函数单个参数的情况是否省略括号
   arrowParens: 'avoid',
   proseWrap: 'preserve',
+  // 对象空格
+  bracketSpacing: true,
   overrides: [
     {
       'files': ['*.md'],
@@ -16,4 +26,4 @@ module.exports = {
       },
     },
   ],
-}
+};
